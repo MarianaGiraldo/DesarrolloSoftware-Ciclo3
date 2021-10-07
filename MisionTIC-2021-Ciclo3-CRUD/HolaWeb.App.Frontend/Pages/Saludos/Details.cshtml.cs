12f4f987bc2542e1c0044631bb3d7aa79076d5d4
MarianaGiraldo <mgiraldo594@misena.edu.co>
2021-10-06 20:24:30 -0500

    trabajo semana 5

--- MisionTIC-2021-Ciclo3-CRUD/HolaWeb.App.Frontend/Pages/Saludos/Details.cshtml.cs
+++ MisionTIC-2021-Ciclo3-CRUD/HolaWeb.App.Frontend/Pages/Saludos/Details.cshtml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-new DateTime(2042, 12, 24)using Microsoft.AspNetCore.Mvc;
+using HolaWeb.App.Dominio;
+using HolaWeb.App.Persistencia.AppRepositorios;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HolaWeb.App.Frontend.Pages
